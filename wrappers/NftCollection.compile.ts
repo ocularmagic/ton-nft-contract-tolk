b5301a30e49bbb5117ff0ceb4bd6f0727c17582a
ocularmagic <38451161+ocularmagic@users.noreply.github.com>
2026-03-23 16:06:20 -0700

    Fix Blueprint Tolk build configuration

--- wrappers/NftCollection.compile.ts
+++ wrappers/NftCollection.compile.ts
@@ -1,5 +1,6 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    targets: ['nft/nft-collection.tolk'],
-}
+    lang: 'tolk',
+    entrypoint: 'nft/nft-collection.tolk',
+};
--- wrappers/NftItem.compile.ts
+++ wrappers/NftItem.compile.ts
@@ -1,5 +1,6 @@
 import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
-    targets: ['nft/nft-item.tolk'],
-}
+    lang: 'tolk',
+    entrypoint: 'nft/nft-item.tolk',
+};
